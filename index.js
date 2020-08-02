bd495655084431c113001521088928adbf14e431
Javier Aranguren <javier@arangurensoto.com>
2020-08-02 19:31:59 -0400

    first commit

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const cors = require('cors')
 
+const PORT = process.env.PORT || 3000;
 const { mongoose } = require('./db')
 var promotionController = require('./controllers/promotionController')
 
 var app = express()
 app.use(bodyParser.json())
 app.use(cors({ origin: 'https://walmartjavieraranguren.herokuapp.com' }))
-app.listen(process.env.PORT || 3000, () => console.log('Server started at port : 3000'))
+app.listen(PORT, () => console.log('Server started at port : 3000'))
 
-app.use('/promotions', promotionController)
+app.use('/', promotionController)
